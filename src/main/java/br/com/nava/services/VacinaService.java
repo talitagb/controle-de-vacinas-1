817cf4a156f833236011dac252052e935ab0daa0
talitagb <talita.t.b001@gmail.com>
2022-05-15 14:11:27 -0300

    @talitagb[alterações camada Service]

--- src/main/java/br/com/nava/services/VacinaService.java
+++ src/main/java/br/com/nava/services/VacinaService.java
@@ -1,6 +1,59 @@
 package br.com.nava.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.nava.dtos.VacinaDTO;
+import br.com.nava.entities.VacinaEntity;
+import br.com.nava.repositories.VacinaRepository;
+
+@Service
 public class VacinaService {
+	@Autowired
+	private VacinaRepository vacinaRepository;
 
+	public List<VacinaDTO> getAll() {
+		List<VacinaEntity> lista = vacinaRepository.findAll();
+		List<VacinaDTO> listaDTO = new ArrayList<>();
+		for (VacinaEntity vacinaEntity : lista) {
+			listaDTO.add(vacinaEntity.toDTO());
+		}
+		return listaDTO;
+	}
+
+	public VacinaDTO getOne(int id) {
+		Optional<VacinaEntity> optional = vacinaRepository.findById(id);
+		VacinaEntity vacina = optional.orElse(new VacinaEntity());
+		return vacina.toDTO();
+	}
+
+	public VacinaDTO save(VacinaEntity vacinaEntity) {
+		return vacinaRepository.save(vacinaEntity).toDTO();
+	}
+
+	public VacinaDTO update(int id, VacinaEntity vacina) {
+		Optional<VacinaEntity> optional = vacinaRepository.findById(id);
+		if (optional.isPresent() == true) {
+			VacinaEntity vacinaBD = optional.get();
+			vacinaBD.setFabricanteVacina(vacina.getFabricanteVacina());
+			vacinaBD.setNomeVacina(vacina.getNomeVacina());
+			vacinaBD.setQntdDiasDoses(vacina.getQntdDiasDoses());
+			vacinaBD.setQntdDoses(vacina.getQntdDoses());
+			vacinaBD.setVacinacao(vacina.getVacinacao());
+			
+
+			return vacinaRepository.save(vacinaBD).toDTO();
+		} else {
+			return new VacinaEntity().toDTO();
+		}
+	}
+	
+	public void delete(int id) {
+		vacinaRepository.deleteById(id);
+		
+	}
 }
--- src/main/java/br/com/nava/services/VacinacaoService.java
+++ src/main/java/br/com/nava/services/VacinacaoService.java
@@ -1,5 +1,60 @@
 package br.com.nava.services;
 
-public class VacinacaoService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.nava.dtos.VacinacaoDTO;
+import br.com.nava.entities.VacinacaoEntity;
+import br.com.nava.repositories.VacinacaoRepository;
+
+@Service
+public class VacinacaoService {
+	@Autowired
+	private VacinacaoRepository vacinacaoRepository;
+
+	public List<VacinacaoDTO> getAll() {
+		List<VacinacaoEntity> lista = vacinacaoRepository.findAll();
+		List<VacinacaoDTO> listaDTO = new ArrayList<>();
+		for (VacinacaoEntity vacinacaoEntity : lista) {
+			listaDTO.add(vacinacaoEntity.toDTO());
+		}
+		return listaDTO;
+	}
+
+	public VacinacaoDTO getOne(int id) {
+		Optional<VacinacaoEntity> optional = vacinacaoRepository.findById(id);
+		VacinacaoEntity vacinacao = optional.orElse(new VacinacaoEntity());
+		return vacinacao.toDTO();
+	}
+
+	public VacinacaoDTO save(VacinacaoEntity vacinacaoEntity) {
+		return vacinacaoRepository.save(vacinacaoEntity).toDTO();
+	}
+
+	public VacinacaoDTO update(int id, VacinacaoEntity vacinacao) {
+		Optional<VacinacaoEntity> optional = vacinacaoRepository.findById(id);
+		if (optional.isPresent() == true) {
+			VacinacaoEntity vacinacaoBD = optional.get();
+			vacinacaoBD.setDataAplic(vacinacao.getDataAplic());
+			vacinacaoBD.setDataProxAplic(vacinacao.getDataProxAplic());
+			vacinacaoBD.setIdUsuario(vacinacao.getIdUsuario());
+			vacinacaoBD.setIdVacina(vacinacao.getIdVacina());
+			vacinacaoBD.setNumDose(vacinacao.getNumDose());
+			vacinacaoBD.setUsuario(vacinacao.getUsuario());
+			vacinacaoBD.setVacina(vacinacao.getVacina());
+
+			return vacinacaoRepository.save(vacinacaoBD).toDTO();
+		} else {
+			return new VacinacaoEntity().toDTO();
+		}
+	}
+
+	public void delete(int id) {
+		vacinacaoRepository.deleteById(id);
+
+	}
 }
