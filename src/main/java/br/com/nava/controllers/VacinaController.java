9ab4d0f77119886a346ebe29315edf8f8513b29d
talitagb <talita.t.b001@gmail.com>
2022-05-15 13:14:38 -0300

    @talitagb[alterações camada Controller]

--- src/main/java/br/com/nava/controllers/VacinaController.java
+++ src/main/java/br/com/nava/controllers/VacinaController.java
@@ -1,5 +1,53 @@
 package br.com.nava.controllers;
 
-public class VacinaController {
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.nava.dtos.VacinaDTO;
+import br.com.nava.services.VacinaService;
+
+@RestController
+@RequestMapping("vacina")
+public class VacinaController {
+	@Autowired
+	private VacinaService vacinaService;
+	
+	@GetMapping()
+	public ResponseEntity<List<VacinaDTO>> getAll(){
+		return ResponseEntity.status(HttpStatus.OK).body(vacinaService.getAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<VacinaDTO> getOne(@PathVariable int id) {
+		return ResponseEntity.status(HttpStatus.OK).body(vacinaService.getOne(id));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<VacinaDTO> save(@Valid @RequestBody VacinaDTO vacina) {
+		return ResponseEntity.status(HttpStatus.OK).body(vacinaService.save(vacina.toEntity()));
+	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<VacinaDTO> update(@PathVariable int id, @RequestBody VacinaDTO vacina) {
+		return ResponseEntity.status(HttpStatus.OK).body(vacinaService.update(id, vacina.toEntity()));
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> delete(@PathVariable int id) {
+		vacinaService.delete(id);
+		return ResponseEntity.ok().build();
+	}
 }
