2e1a84dfe8af805f81b825c919123ee823e666b0
talitagb <talita.t.b001@gmail.com>
2022-05-16 16:17:00 -0300

    @talitagb[altrações camada Controller]

--- src/main/java/br/com/nava/controllers/UsuarioController.java
+++ src/main/java/br/com/nava/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,8 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.nava.dtos.UsuarioDTO;
 import br.com.nava.services.UsuarioService;
 
+
 @RestController
 @RequestMapping("usuarios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
